f5384bf723f6af995f307f6f578f21d7de758a54
Prem Kumar <premkumaropm182@gmail.com.com>
2025-07-02 09:52:51 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const User = require("./models/User");
+const User = require("./models/user.js");
 
 dotenv.config();
 const app = express();
